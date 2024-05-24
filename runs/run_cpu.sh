083ac77aa08734768d9699a194364c2d7735f606
u219333 <u219333@login-2.aidevcloud>
2024-05-24 01:23:03 -0700

    added vtune

--- runs/run_cpu.sh
+++ runs/run_cpu.sh
@@ -2,4 +2,8 @@
 source /opt/intel/inteloneapi/setvars.sh > /dev/null 2>&1
 cd ../NPB-CPP/
 make sp CLASS=$class_type
-./bin/sp.$class_type
+echo "hi"
+echo $HOSTNAME
+vtune -collect performance-snapshot -collect hotspots ./bin/sp.$class_type
+# ./bin/sp.$class_type
+echo "DONE"
